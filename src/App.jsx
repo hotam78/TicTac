ba8c340eb0fe5b10313c0bcb9feccca26db19768
hotam <hotamtor@gmail.com>
2024-05-05 12:21:13 +0300

    start

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import Button from './components/button'
+import Board from './components/board'
+import X from './components/x'
+import O from './components/o'
+
+function App() {
+
+  return (
+    <>
+      <h1>Tic Tac Teo</h1>
+      <Button/>
+      <Board/>
+      <X/>
+      <O/>
+    </>
+  )
+}
+
+export default App
---
+++ src/components/o/index.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function O() {
+  return (
+    <div>O</div>
+  )
+}
