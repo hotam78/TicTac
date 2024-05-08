1d5db6ba0bb3e10df9e6abdc7ec7c28fd2f74501
hadasa <hadasaeti@gmail.com>
2024-05-08 12:40:17 +0300

    choosePlayer

--- src/components/board/index.jsx
+++ src/components/board/index.jsx
@@ -2,10 +2,11 @@ import React from 'react'
 import styles from './style.module.scss'
 
 
-export default function Board({ title, height }) {
+export default function Board({ title, children }) {
   return (
-    <div className={styles.board} style={{ height }}>
+    <div className={styles.board} >
       <div className={styles.title}>{title}</div>
+      {children}
     </div>
   )
 }
